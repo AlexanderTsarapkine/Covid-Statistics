9ae3fe8c6dd7725f24275bb00aaffea0903d10a2
Alexander Tsarapkine <alexander.tsarapkine@gmail.com>
2020-07-16 00:47:59 -0400

    added global stats

--- main.js
+++ main.js
@@ -1,18 +1,19 @@
 const fetch = require("node-fetch");
 
 
-// Gets object storing data from each country from API
+// Gets object storing data from each country and globally from API
 const url = 'https://api.covid19api.com/summary';
+
+let countryStat = [];
+let globalStat;
 
 fetch(url)
 .then((res) => res.json())
 .then((data) => {
-
- let countryStats = [];
  data.Countries.forEach(function(country) {
-  countryStats.push(country);
+  countryStat.push(country);
  });
-
- console.log(countryStats);
-  
+ console.log(countryStat);
+ globalStat = data.Global;
+  console.log(globalStat);
 })
