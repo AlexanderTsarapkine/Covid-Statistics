e1e22d6a7e6dbe8e5ee647a71b691ba218546e4a
Alexander Tsarapkine <alexander.tsarapkine@gmail.com>
2020-07-16 00:34:56 -0400

    Receive and store data from each country

--- main.js
+++ main.js
@@ -1,9 +1,18 @@
 const fetch = require("node-fetch");
 
-const url = 'https://api.covid19api.com/world/total';
+
+// Gets object storing data from each country from API
+const url = 'https://api.covid19api.com/summary';
 
 fetch(url)
 .then((res) => res.json())
 .then((data) => {
-  console.log(data);
+
+ let countryStats = [];
+ data.Countries.forEach(function(country) {
+  countryStats.push(country);
+ });
+
+ console.log(countryStats);
+  
 })
