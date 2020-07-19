5466be273ec685df553a1adfd00cfe5a10c316a1
lukagra <lukagrahek2003@gmail.com>
2020-07-19 00:53:09 -0400

    Added css file and sorting buttons

--- makeTable.js
+++ makeTable.js
@@ -4,12 +4,12 @@ let generatedRestAPI = 'http://3.19.229.184/';
 fetch(sourceApi)
 .then((res) => res.json())
 .then((data) => {
-  console.log(data);
+ 
   function generateTable(table, data) {
     for (let element of data) {
       let row = table.insertRow();
       for (key in element) {
-        console.log(key);
+       
         if(key === 'Country' || key === 'TotalConfirmed' || key === 'TotalDeaths' || key === 'TotalRecovered'){
           let cell = row.insertCell();
           let text = document.createTextNode(element[key]);
@@ -19,7 +19,11 @@ fetch(sourceApi)
     }
   }
   
-  let table = document.querySelector("table");
+  let table = document.getElementById("countryTable");
   generateTable(table , data.Countries);
 
 })
+
+function getCountryData(){
+  return data.Countries;
+}
